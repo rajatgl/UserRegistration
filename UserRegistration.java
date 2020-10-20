31ef9fe949d3a918f965eece8448f74d05ebfa2e
Rajat Gundi <glrajat@gmail.com>
2020-10-20 13:17:42 +0530

    Regex for Phone

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,6 +4,7 @@ class UserRegistrationUtil
   public static final String FIRST_NAME_PATTERN = "[A-Z]{1}[a-z]{2,}";
   public static final String LAST_NAME_PATTERN = "[A-Z]{1}[a-z]{2,}";
   public static final String EMAIL_PATTERN = "[a-z0-9]{3,15}(|[.]?[a-z0-9]+)@[a-z0-9]{1,15}(.[a-z]{2,4})(|[.]?[a-z]{2,4})";
+  public static final String PHONE_PATTERN = "[0-9]{2,3}[\\s][0-9]{10}";
   public boolean match(String str, String pattern){
     return str.matches(pattern);
   }
@@ -38,6 +39,14 @@ public class UserRegistration
     }
     else{
       System.out.println("Invalid Email");
+    }
+    System.out.print("Phone: ");
+    String email = sc.nextLine();
+    if(util.match(email, UserRegistrationUtil.PHONE_PATTERN)){
+      System.out.println("Valid Phone");
+    }
+    else{
+      System.out.println("Invalid Phone");
     }
   }
 }
