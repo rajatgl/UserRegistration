2d13ae5dbc9632eb0fad4d53903616f050a8f3f6
Rajat Gundi <glrajat@gmail.com>
2020-10-21 12:28:08 +0530

    fix: Phone Number Validator Rectified

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,6 +5,7 @@ class UserRegistrationUtil
   public static final String LAST_NAME_PATTERN = "[A-Z]{1}[a-z]{2,}";
   public static final String EMAIL_PATTERN = "[a-z0-9]{3,15}(|[.]?[a-z0-9]+)@[a-z0-9]{1,15}(.[a-z]{2,4})(|[.]?[a-z]{2,4})";
   public static final String PHONE_PATTERN = "[0-9]{2,3}[\\s][0-9]{10}";
+  public static final String PASSWORD_PATTERN = "^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?=.*[@#$%^&+=])" + "(?=\\S+$).{8,20}$";
   public boolean match(String str, String pattern){
     return str.matches(pattern);
   }
@@ -41,8 +42,8 @@ public class UserRegistration
       System.out.println("Invalid Email");
     }
     System.out.print("Phone: ");
-    String email = sc.nextLine();
-    if(util.match(email, UserRegistrationUtil.PHONE_PATTERN)){
+    String phone = sc.nextLine();
+    if(util.match(phone, UserRegistrationUtil.PHONE_PATTERN)){
       System.out.println("Valid Phone");
     }
     else{
